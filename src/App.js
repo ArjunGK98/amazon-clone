e09ea7135912189353edc6529644fac16f33037f
Mallikarjun GK <mallikarjungk98@gmail.com>
2021-10-28 10:23:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          {
+            <Route path="/">
+              <Header />
+              <Home />
+            </Route>
+          }
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
